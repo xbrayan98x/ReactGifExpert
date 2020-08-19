e3e6b38aac988f01925955a7cba9991319a84fe9
brayan <brayanrojas113@gmail.com>
2020-08-19 12:03:36 -0500

    distribucion

---
+++ docs/precache-manifest.e8ca1a9bb96e5136b0fb5f5fe596b36f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33502ab0924c68ecb1c9046784fccfa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5816c841222e375554a4",
+    "url": "/static/css/main.ab3f61fd.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5816c841222e375554a4",
+    "url": "/static/js/main.f1168282.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);

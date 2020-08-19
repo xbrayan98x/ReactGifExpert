b21ac033ad2c31a6e73555a8b84c6527a127a883
brayan <brayanrojas113@gmail.com>
2020-08-19 12:28:38 -0500

    solution test 404

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,18 +1,17 @@
 
-export const getGifs = async( categoria ) =>{
+export const getGifs = async( categoria ) => {
 
-    const api_key = 'KMYJgPaBHpV7IiEBaCJJWlzKnIDaKTHh';
-    const urlEndPoint = `https:/api.giphy.com/v1/gifs/search?q=${ encodeURI( categoria ) }&limit=10&api_key=${  api_key }`;
-    const resp = await fetch( urlEndPoint );
+    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( categoria ) }&limit=10&api_key=KMYJgPaBHpV7IiEBaCJJWlzKnIDaKTHh`;
+    const resp = await fetch( url );
     const { data } = await resp.json();
-    
+
     const gifs = data.map( img => {
         return {
             id: img.id,
             title: img.title,
-            url: img.images?.downsized_medium.url,
+            url: img.images?.downsized_medium.url
         }
-    });
+    })
 
-   return gifs;
+    return gifs;
 }
